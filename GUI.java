09bbc63a423ae1b756ff4f5d589dbebf3088528f
ay4ssin <164530396+ay4ssin@users.noreply.github.com>
2024-07-14 19:26:56 +0100

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+public class GUI extends Main implements ActionListener {
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
     JButton button1 = new JButton("Create a new account");
@@ -30,16 +30,128 @@ public class GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == button1) {
-            // new SignupGUI;
+            frame.setVisible(false);
+            JFrame signupFrame = new JFrame();
+            JPanel signup = new JPanel();
+            JLabel nameLabel = new JLabel("Full Name");
+            JLabel depositNumber = new JLabel("Deposit");
+            JTextField userName = new JTextField(20);
+            JTextField initialDeposit = new JTextField(20);
+
+            signup.setBorder(BorderFactory.createEmptyBorder(80, 160, 80, 160));
+            signup.setLayout(new GridLayout(3, 2));
+
+            nameLabel.setBounds(10,20,80,25);
+            signup.add(nameLabel);
+
+            userName.setBounds(100,20,165,25);
+            signup.add(userName);
+
+            depositNumber.setBounds(10,20,80,25);
+            signup.add(depositNumber);
+
+            initialDeposit.setBounds(100,20,165,25);
+            signup.add(initialDeposit);
+
+            JButton signupButton = new JButton("Signup");
+            signupButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    /* if (strawberry.accountExists(accountNum)) {
+                        JLabel blank = new JLabel("");
+                        blank.setBounds(100,50,165,25);
+                        login.add(blank);
+                        login.add(success);
+                        success.setForeground(Color.GREEN);
+                        success.setText("Successfully Logged In");
+                        mainGUI();
+                    } else {
+                        JLabel blank = new JLabel("");
+                        blank.setBounds(100,50,165,25);
+                        login.add(blank);
+                        login.add(success);
+                        success.setForeground(Color.RED);
+                        success.setText("Incorrect Details, Please try again");
+                    } */
+                }
+            });
+            signupButton.setBounds(10, 40, 40, 25);
+            signupButton.addActionListener(this);
+            signup.add(signupButton);
+
+            signupFrame.add(signup);
+            signupFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            signupFrame.setTitle("Signup");
+            signupFrame.pack();
+            signupFrame.setVisible(true);
         } else {
             frame.setVisible(false);
-            new LoginGUI();
+            JFrame loginFrame = new JFrame();
+            JPanel login = new JPanel();
+            JLabel nameLabel = new JLabel("Full Name");
+            JLabel accountLabel = new JLabel("Account Number");
+            JTextField userName = new JTextField(20);
+            JPasswordField accountNumber = new JPasswordField(20);
+
+            login.setBorder(BorderFactory.createEmptyBorder(80, 160, 80, 160));
+            login.setLayout(new GridLayout(4, 2));
+
+            nameLabel.setBounds(10,20,80,25);
+            login.add(nameLabel);
+
+            userName.setBounds(100,20,165,25);
+            login.add(userName);
+
+            accountLabel.setBounds(10,50,80,25);
+            login.add(accountLabel);
+
+            accountNumber.setBounds(100,50,165,25);
+            login.add(accountNumber);
+
+            JLabel success = new JLabel("");
+            success.setBounds(100,50,165,25);
+
+            JLabel blank = new JLabel("");
+            blank.setBounds(100,50,165,25);
+            login.add(blank);
+
+            JButton loginButton = new JButton("Login");
+            loginButton.setBounds(10, 80, 80, 25);
+            loginButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String accountNum = accountNumber.getText();
+                    if (strawberry.accountExists(accountNum)) {
+                        JLabel blank = new JLabel("");
+                        blank.setBounds(100,50,165,25);
+                        login.add(blank);
+                        login.add(success);
+                        success.setForeground(Color.GREEN);
+                        success.setText("Successfully Logged In");
+                        mainGUI();
+                    } else {
+                        JLabel blank = new JLabel("");
+                        blank.setBounds(100,50,165,25);
+                        login.add(blank);
+                        login.add(success);
+                        success.setForeground(Color.RED);
+                        success.setText("Incorrect Details, Please try again");
+                    }
+                }
+            });
+            login.add(loginButton);
+            loginFrame.add(login);
+            loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            loginFrame.setTitle("Login");
+            loginFrame.pack();
+            loginFrame.setVisible(true);
         }
 
     }
 
+    public void mainGUI() {
+
+    }
 
     public static void main(String[] args) {
         new GUI();
     }
-} 
+}
