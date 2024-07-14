f4d716cb5667fe941f10c4e13411f0ba2f1ae5a9
ay4ssin <164530396+ay4ssin@users.noreply.github.com>
2024-07-14 19:27:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    protected Bank strawberry = new Bank();
+
+
     public static void main(String[] args) {
-        // new GUI();
+
     }
 }
